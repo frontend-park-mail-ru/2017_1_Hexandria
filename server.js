89f5219840970e1d4387221c94577677af4fa309
Alexandr Orel <4aorel@mail.ru>
2017-02-16 22:52:35 +0300

    Init

---
+++ server.js
@@ -0,0 +1,52 @@
+'use strict';
+
+let express = require('express');
+let app = express();
+const staticPath = __dirname + '/public';
+
+app.disable('x-powered-by');
+
+app.use(function(req, res, next){
+    console.log('%s %s', req.method, req.url);
+    next();
+});
+
+app.use(express.static(staticPath));
+
+app.get('/', function(req, res){
+    res.sendFile('index.html', {root: staticPath});
+});
+
+app.get('/login', function(req, res){
+    res.sendFile('login.html', {root: staticPath});
+});
+
+app.get('/signup', function(req, res){
+    res.sendFile('signup.html', {root: staticPath});
+});
+
+app.get('/scoreboard', function(req, res){
+    res.sendFile('scoreboard.html', {root: staticPath});
+});
+
+app.get('/singleplayer', function(req, res){
+    res.sendFile('singleplayer.html', {root: staticPath});
+});
+
+app.get('/multiplayer', function(req, res){
+    res.sendFile('multiplayer.html', {root: staticPath});
+});
+
+app.get('/about', function(req, res){
+    res.sendFile('about.html', {root: staticPath});
+});
+
+app.get('/test', function(req, res){
+    res.sendFile('test.html', {root: staticPath});
+});
+
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, function(){
+    console.log('Listening on port %s', port);
+})
