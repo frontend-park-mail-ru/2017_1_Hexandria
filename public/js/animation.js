3aa3f98040a1172c296fdccac66fc7b80a303da7
trntga <trntga@gmail.com>
2017-02-22 12:04:36 +0300

    About and Scoreboard added to SPA

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,16 +1,33 @@
+'use strict';
+
+// Получаем ссылки на элементы страницы
+const page_index = document.getElementById('page_index');
+const page_about = document.getElementById('page_about');
+const page_scoreboard = document.getElementById('page_scoreboard');
+
+// Элементы страницы page_index
+const main_hex = document.getElementById('hex');
+const minor_hex = document.getElementById('hex__minor_hex');
+const logo = document.getElementById('hex__title');
+const motto = document.getElementById('hex__motto');
+const buttons = document.getElementById('buttons');
+const registration = document.getElementById('registration');
+const button_about = document.getElementById('button_about');
+const button_scoreboard = document.getElementById('button_scoreboard');
+
+// Элементы страницы page_about
+const button_back_from_about = document.getElementById('button_back_from_about');
+
+// Элементы страницы page_scoreboard
+const button_back_from_scoreboard = document.getElementById('button_back_from_scoreboard');
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-window.onload = async function() {
-    let main_hex = document.getElementById('hex');
-    let minor_hex = document.getElementById('hex__minor_hex');
-    let logo = document.getElementById('hex__title');
-    let motto = document.getElementById('hex__motto');
-    let buttons = document.getElementById('buttons');
-    let registration = document.getElementById('registration');
+async function main_animation() {
 
-    await sleep(4100);
+    // await sleep(4100);
 
     main_hex.style.opacity = "0";
     logo.style.opacity = "0";
@@ -31,16 +48,53 @@ window.onload = async function() {
         logo.style.opacity = i.toString();
     }
 
-    await sleep(1350);
+    // await sleep(1350);
     for(let i = 0; i < 1; i += 0.01) {
         await sleep(25);
         motto.style.opacity = i.toString();
     }
 
-    await sleep(1350);
+    // await sleep(1350);
     for(let i = 0; i < 1; i += 0.01) {
         await sleep(25);
         registration.style.opacity = i.toString();
         buttons.style.opacity = i.toString();
     }
+}
+
+window.onload = function() {
+
+    // Скрываем все страницы, кроме главной
+    page_about.hidden = true;
+    page_scoreboard.hidden = true;
+
+    main_animation();
 };
+
+// Добавляем обработчик события на кнопку "about"
+button_about.addEventListener('click', function (event) {
+    // Переключаем страницы
+    page_index.hidden = true;
+    page_about.hidden = false;
+});
+
+// Добавляем обработчик события на кнопку "about"
+button_scoreboard.addEventListener('click', function (event) {
+    // Переключаем страницы
+    page_index.hidden = true;
+    page_scoreboard.hidden = false;
+});
+
+// Добавляем обработчик события на кнопку "back"
+button_back_from_about.addEventListener('click', function (event) {
+    // Переключаем страницы
+    page_index.hidden = false;
+    page_about.hidden = true;
+});
+
+// Добавляем обработчик события на кнопку "back"
+button_back_from_scoreboard.addEventListener('click', function (event) {
+    // Переключаем страницы
+    page_index.hidden = false;
+    page_scoreboard.hidden = true;
+});
--- server.js
+++ server.js
@@ -15,40 +15,10 @@ app.use(express.static(staticPath));
 app.use('/lib', express.static('node_modules'));
 
 
-app.get('/', function(req, res){
-    res.sendFile('index.html', {root: staticPath});
-});
-
-app.get('/login', function(req, res){
-    res.sendFile('login.html', {root: staticPath});
-});
-
-app.get('/signup', function(req, res){
-    res.sendFile('signup.html', {root: staticPath});
-});
-
-app.get('/scoreboard', function(req, res){
-    res.sendFile('scoreboard.html', {root: staticPath});
-});
-
-app.get('/singleplayer', function(req, res){
-    res.sendFile('singleplayer.html', {root: staticPath});
-});
-
-app.get('/multiplayer', function(req, res){
-    res.sendFile('multiplayer.html', {root: staticPath});
-});
-
-app.get('/about', function(req, res){
-    res.sendFile('about.html', {root: staticPath});
-});
-
-app.get('/test', function(req, res){
-    res.sendFile('test.html', {root: staticPath});
-});
+app.use('/', express.static('public'));
 
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, function(){
     console.log('Listening on port %s', port);
-})
+});
