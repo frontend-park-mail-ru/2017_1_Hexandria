69d191d027c5d6423e888ccc4bb7237d6d822e7f
trntga <trntga@gmail.com>
2017-03-03 23:14:02 +0300

    Login form half-decorated

--- public/js/form.js
+++ public/js/form.js
@@ -1,71 +1,73 @@
 // Constants
 const fetcher = new Fetcher();
+const validator = new Validator();
 
 // Design functions
 const colorInputBorder = function(input, color) {
     input.style.border = `1px solid ${color}`;
 };
 
-document.forms["form_login"]["login"].addEventListener('blur', function() {
-    if (!this.value || this.value === null) {
-        document.getElementById('error_login').textContent = 'Enter something!';
-        colorInputBorder(this, "red");
-        return;
-    }
-    if (!this.value.match(/^[a-zA-Z0-9]{1,10}$/)) {
-        document.getElementById('error_login').textContent = 'Only latin letters and numerals!';
-        colorInputBorder(this, "red");
-        return;
-    }
-    colorInputBorder(this, "#fff");
-});
+(function() {
+    // Constants
+    const form = document.forms["form_login"];
+    const login = form["login"];
+    const password = form["password"];
+    const error_login = document.getElementById('error_login');
 
+    // Erase error
+    for (let i = 0; i < form.length; i++) {
+        form[i].addEventListener('focus', function() {
+            this.nextSibling.nextSibling.textContent = '';
+            colorInputBorder(this, "#eb9300");
+        });
+    }
 
-document.forms["form_login"]["password"].addEventListener('blur', function() {
-    if (!this.value || this.value === null) {
-        document.getElementById('error_password').textContent = 'Enter something!';
-        colorInputBorder(this, "red");
-        return;
+    function inputBlurDecorator(errorField, validateFunction) {
+        return function() {
+            try {
+                validateFunction(this);
+            }
+            catch(err) {
+                console.log(err);
+                document.getElementById(errorField).textContent = err.name;
+                colorInputBorder(this, "red");
+                return;
+            }
+            colorInputBorder(this, "#fff");
+        }
     }
-    if (!this.value.match(/^[a-zA-Z0-9]{1,10}$/)) {
-        document.getElementById('error_password').textContent = 'Only latin letters and numerals!';
-        colorInputBorder(this, red);
-        return;
-    }
-    if(this.value.length < 5) {
-        document.getElementById('error_password').textContent = 'Enter more than 4 symbols!';
-        colorInputBorder(this, "red");
-        return;
-    }
-    colorInputBorder(this, "#fff");
-});
+    login.addEventListener('blur', inputBlurDecorator('error_login', validator.validateLogin));
 
-// Erase error
-const inputs = document.forms["form_login"];
-for (let i = 0; i < inputs.length; i++) {
-    inputs[i].addEventListener('focus', function() {
-        console.log(this.nextSibling.nextSibling);
-        this.nextSibling.nextSibling.textContent = '';
-        colorInputBorder(this, "#eb9300");
+    password.addEventListener('blur', function() {
+        try {
+            validator.validateLogin(this);
+        }
+        catch(err) {
+            document.getElementById('error_login').textContent = err.name;
+            colorInputBorder(this, "red");
+            return;
+        }
+        colorInputBorder(this, "#fff");
+        colorInputBorder(this, "#fff");
     });
-}
 
-document.getElementById('login_submit').addEventListener('click', function() {
-    console.log("button_login click");
+    document.getElementById('login_submit').addEventListener('click', function() {
+        console.log("button_login click");
 
-    const parent = this.parentNode;
-    const data = {
-        login: parent['login'].value,
-        password: parent['password'].value,
-    };
+        const parent = this.parentNode;
+        const data = {
+            login: parent['login'].value,
+            password: parent['password'].value,
+        };
 
-    fetcher.fetch('/api/login', 'POST', data)
-        .then(function(okJSON) {
-            console.log(okJSON);
-            console.log(okJSON.description);
-        })
-        .catch(errorCatcher);
-});
+        fetcher.fetch('/api/login', 'POST', data)
+            .then(function(okJSON) {
+                console.log(okJSON);
+                console.log(okJSON.description);
+            })
+            .catch(errorCatcher);
+    });
+})();
 
 // Dolan is here
 document.getElementById('signup_submit').addEventListener('click', function() {
--- public/js/validator.js
+++ public/js/validator.js
@@ -1,10 +1,12 @@
-// Constants
-const minLength = 4;
-const maxLength = 10;
-const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
+'use strict';
 
 const Validator = function() {
+    // Constants
+    const minLength = 4;
+    const maxLength = 10;
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
+
     this.checkEmpty = function(input) {
         if (!input.value || input.value === null) {
             throw new SyntaxError("empty");
@@ -37,84 +39,20 @@ const Validator = function() {
         this.checkMaxLength(input);
         this.checkEmail(input);
     };
+    this.validateLogin = function(input) {
+        this.checkEmpty(input);
+        this.checkMinLength(input);
+        this.checkMaxLength(input);
+    };
     this.validatePassword = function(input) {
         this.checkEmpty(input);
         this.checkMinLength(input);
         this.checkMaxLength(input);
 
     };
-    this.validateLogin = function(input) {
-        this.checkEmpty(input);
-        this.checkMinLength(input);
-        this.checkMaxLength(input);
-    };
-
-    this.validateAll = function(form) {
-        let logins = form.getElementsByName('login');
-        for (let i = 0; i < logins.length; i++) {
-            this.validateLogin(logins[i]);
-        }
-
-        let emails = form.getElementsByName('email');
-        for (let i = 0; i < emails.length; i++) {
-            this.validateEmail(emails[i]);
-        }
-
-        let passwords = form.getElementsByName('password');
-        for (let i = 0; i < passwords.length; i++) {
-            this.validatePassword(passwords[i]);
+    this.validateDoublePasswords = function(first, second) {
+        if (first.value === second.value) {
+            throw new SyntaxError("not equal passwords");
         }
     };
-
-
-    this.check = function(form) {
-        if (arguments.length < 2) {
-            return false;
-        }
-        //console.log(arguments.length);
-
-        validationResult = true;
-        for (var i = 1; i < arguments.length; i++) {
-            //console.log(arguments[i]);
-
-            if (arguments[i] === 'login') {
-                _checkEmpty(form, arguments[i]);
-            } else if (arguments[i] === 'password') {
-                _checkEmpty(form, arguments[i]);
-            } else if (arguments[i] === 'email') {
-                _checkEmpty(form, arguments[i]);
-            } else {
-                return false;
-            }
-        }
-        if (validationResult) {
-            _errorMessage(form, '');
-        }
-        return validationResult;
-    }
-
-    let validationResult;
-
-    function _checkEmpty(form, element) {
-        if (validationResult) {
-            const value = form[element].value;
-            if (value && typeof value === 'string') {
-                //ok
-            } else {
-                _errorMessage(form, 'Заполните поле ' + element);
-                validationResult = false;
-            }
-        }
-    }
-    function _errorMessage(form, message) {
-        let elements = form.getElementsByClassName("errors");
-        if (elements.length === 0) {
-            let div = document.createElement('div');
-            div.className = "errors";
-            div.innerHTML = message;
-            form.appendChild(div);
-        } else {
-            elements[0].innerHTML = message;
-        }
-    }
-}
+};
