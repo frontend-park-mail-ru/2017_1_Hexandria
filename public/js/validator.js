a56a74ee661258e257a55aa61386506b6fb8f4e7
trntga <trntga@gmail.com>
2017-03-03 22:21:23 +0300

    Validator methods finished

--- public/js/validator.js
+++ public/js/validator.js
@@ -1,23 +1,52 @@
+// Constants
+const minLength = 4;
+const maxLength = 10;
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
+
 const Validator = function() {
-
     this.checkEmpty = function(input) {
-
+        if (!input.value || input.value === null) {
+            throw new SyntaxError("empty");
+        }
     };
-
-    this.checkLength = function(input) {
-
+    this.checkMinLength = function(input) {
+        if (input.value.length < minLength) {
+            throw new SyntaxError("too short");
+        }
+    };
+    this.checkMaxLength = function(input) {
+        if (input.value.length > maxLength) {
+            throw new SyntaxError("too long");
+        }
+    };
+    this.checkEmail = function(input) {
+        if (!emailRegex.test(input.value)){
+            throw new SyntaxError("not email");
+        }
+    };
+    this.checkPassword = function(input) {
+        if (!passwordRegex.test(input.value)){
+            throw new SyntaxError("not password");
+        }
     };
 
     this.validateEmail = function(input) {
         this.checkEmpty(input);
+        this.checkMinLength(input);
+        this.checkMaxLength(input);
+        this.checkEmail(input);
     };
-
     this.validatePassword = function(input) {
         this.checkEmpty(input);
-    };
+        this.checkMinLength(input);
+        this.checkMaxLength(input);
 
+    };
     this.validateLogin = function(input) {
         this.checkEmpty(input);
+        this.checkMinLength(input);
+        this.checkMaxLength(input);
     };
 
     this.validateAll = function(form) {
