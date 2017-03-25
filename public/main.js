b42ad39d49415ad3fad974248a8ae0b5d0000888
Alexandr Orel <4aorel@mail.ru>
2017-03-25 10:46:48 +0300

    main.js refactoring

---
+++ public/main.js
@@ -0,0 +1,24 @@
+;(function() {
+    "use strict";
+
+    const Router = window.Router;
+
+    const SingleplayerView = window.SingleplayerView;
+    const MultiplayerView = window.MultiplayerView;
+    const AboutView = window.AboutView;
+    const ScoreboardView = window.ScoreboardView;
+    const LoginView = window.LoginView;
+    const SignupView = window.SignupView;
+    const MainView = window.MainView;
+
+
+    window.router = (new Router)
+        // .addRoute('/singleplayer', SingleplayerView)
+        // .addRoute('/multiplayer', MultiplayerView)
+        // .addRoute('/about', AboutView)
+        // .addRoute('/scoreboard', ScoreboardView)
+        .addRoute('/login', LoginView)
+        .addRoute('/signup', SignupView)
+        .addRoute('/', MainView)
+        .start();
+})();
