571215bfec5ff74d74b1b7103a7ad38575d450f9
Alexandr Orel <4aorel@mail.ru>
2017-04-07 11:20:43 +0300

    fetcher problem

--- public/hexandria/api.js
+++ public/hexandria/api.js
@@ -17,16 +17,16 @@ api.code = {
 api.auth = {
     login: "Successfully authorized user test-user",
     logout: "User successfully logged out",
-    error: "User not authorized in this session!",
+    error: "UserEntity not authorized in this session!",
     already: "User already authorized in this session",
     signup: "Successfully registered user",
 };
 
 api.path = {
     user: "/api/user",
-    login: "/api/login",
-    logout: "/api/logout",
-    signup: "/api/signup",
+    login: "/api/user/login",
+    logout: "/api/user/logout",
+    signup: "/api/user/signup",
 };
 
 export default api;
